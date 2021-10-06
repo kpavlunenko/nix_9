50bd4f1d97b74f98f06c1c75b4eff9efc9a692e3
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-10-06 16:04:12 +0300

    test commit1

--- src/main/java/ua/com/test/Main.java
+++ src/main/java/ua/com/test/Main.java
@@ -1,9 +1,9 @@
-package ua.com;
+package ua.com.test;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.println("Hello World!");
     }
 
 }
