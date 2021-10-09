d48e31b004fa0a2d5d2cd87a614df04847aa3949
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-10-09 18:00:36 +0300

    task_1

--- hw_1_base_operations/src/main/java/ua/com/alevel/SumOfNumbersInARow.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/SumOfNumbersInARow.java
@@ -1,9 +1,22 @@
 package ua.com.alevel;
 
+import java.util.Scanner;
+
 public class SumOfNumbersInARow {
 
     public static void main(String[] args) {
 
-        System.out.println("111");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the row with numbers:");
+        String inputRow = input.nextLine();
+        String onlyNumberInRow = inputRow.replaceAll("[^0-9]", "");
+
+        int sum = 0;
+        for(int i = 0; i < onlyNumberInRow.length(); i++){
+            sum = sum + Character.getNumericValue(onlyNumberInRow.charAt(i));
+        }
+
+        System.out.println("sum = " + sum);
+
     }
 }
