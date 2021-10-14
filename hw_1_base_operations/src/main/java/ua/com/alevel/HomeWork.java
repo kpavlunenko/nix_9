d6daf75b21b376298b5696d2255d951878ac2d44
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-10-14 11:22:08 +0300

    add restart program in class HomeWork

--- hw_1_base_operations/src/main/java/ua/com/alevel/HomeWork.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/HomeWork.java
@@ -7,10 +7,16 @@ import java.io.InputStreamReader;
 public class HomeWork {
 
     public static void main(String[] args) {
+        runProgram();
+    }
 
+    public static void runProgram() {
+
+        System.out.println("-----------------------------------");
         System.out.println("1. Sum of numbers in row;");
         System.out.println("2. Number of duplicate characters");
         System.out.println("3. Find out the end of the lesson");
+        System.out.println("0. Exit from program");
         System.out.println("Enter number of task:");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -28,14 +34,17 @@ public class HomeWork {
             case 1:
                 RowsFunction rowSum = new RowsFunction();
                 rowSum.sumOfNumbers();
+                runProgram();
                 break;
             case 2:
                 RowsFunction rowDuplicate = new RowsFunction();
                 rowDuplicate.numberOfDuplicateCharacters();
+                runProgram();
                 break;
             case 3:
                 EndOfLessons thirdTask = new EndOfLessons();
                 thirdTask.findOutTheEndOfTheLesson();
+                runProgram();
                 break;
             default:
                 break;
