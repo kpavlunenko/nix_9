0fa01b9f9108689fb9abde9e21d178d9dbf19c5b
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-10-17 20:22:07 +0300

    create reverse functions

--- hw_2_strings/src/main/java/ua/com/alevel/UsersInterface.java
+++ hw_2_strings/src/main/java/ua/com/alevel/UsersInterface.java
@@ -1,9 +1,37 @@
 package ua.com.alevel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class UsersInterface {
 
     public static void main(String[] args) {
 
+        runProgram();
+    }
+
+    public static void runProgram() {
+
+        System.out.println("You can specify substring or start and end indices to reverse (optional)");
+        System.out.println("Optional parameters are entered with a separator \",\"");
+        System.out.println("Enter row which you want to reverse and press enter:");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputRow = "";
+
+        try {
+            inputRow = bufferedReader.readLine();
+
+        } catch (NumberFormatException | IOException e) {
+            System.out.println("Incoming data is incorrect");
+            e.printStackTrace();
+        }
+
+       String reversString = StringFunctions.reverseMethodDefinition(inputRow);
+
+        System.out.println(reversString);
 
     }
 }
