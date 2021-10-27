b5594ec6caecab6ab5ed0d9031d28ea766b74ea1
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-10-27 17:14:19 +0300

    added user interface

---
+++ module_1/src/main/java/ua/com/alevel/ModuleOneMain.java
@@ -0,0 +1,10 @@
+package ua.com.alevel;
+
+import ua.com.alevel.controller.InterfaceController;
+
+public class ModuleOneMain {
+
+    public static void main(String[] args) {
+        new InterfaceController().run();
+    }
+}
---
+++ module_1/src/main/java/ua/com/alevel/controller/InterfaceController.java
@@ -0,0 +1,134 @@
+package ua.com.alevel.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InterfaceController {
+
+    public void run() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ModuleInterface(reader);
+    }
+
+    private void ModuleInterface(BufferedReader reader) {
+        String position;
+        try {
+            runModuleNavigation();
+            while ((position = reader.readLine()) != null) {
+                levelOfModule(position, reader);
+                position = reader.readLine();
+                if (position.equals("0")) {
+                    System.exit(0);
+                }
+                levelOfModule(position, reader);
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void runModuleNavigation() {
+        System.out.println();
+        System.out.println("Enter level of module:");
+        System.out.println("to choose  level 1, please enter 1");
+        System.out.println("to choose  level 2, please enter 2");
+        System.out.println("to choose  level 3, please enter 3");
+        System.out.println("if you want exit, please enter 0");
+        System.out.println();
+    }
+
+    private void levelOfModule(String position, BufferedReader reader) {
+        switch (position) {
+            case "1":
+                LevelOneInterface(reader);
+                break;
+            case "2":
+                LevelTwoInterface(reader);
+                break;
+            case "3":
+                LevelOneInterface(reader);
+                break;
+            case "0":
+                System.exit(0);
+                break;
+        }
+        runModuleNavigation();
+    }
+
+    private void LevelOneInterface(BufferedReader reader) {
+        String position;
+        try {
+            runLevelOneNavigation();
+            while ((position = reader.readLine()) != null) {
+                if (position.equals("0")) {
+                    ModuleInterface(reader);
+                }
+                numberOfTaskLevelOne(position, reader);
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void LevelTwoInterface(BufferedReader reader) {
+        String position;
+        try {
+            runLevelTwoNavigation();
+            while ((position = reader.readLine()) != null) {
+                if (position.equals("0")) {
+                    ModuleInterface(reader);
+                }
+                numberOfTaskLevelTwo(position, reader);
+            }
+        } catch (IOException e) {
+            System.out.println("problem: = " + e.getMessage());
+        }
+    }
+
+    private void runLevelOneNavigation() {
+        System.out.println();
+        System.out.println("Enter task number of level 1:");
+        System.out.println("to choose  task 1, please enter 1");
+        System.out.println("to choose  task 2, please enter 2");
+        System.out.println("to choose  task 3, please enter 3");
+        System.out.println("if you want exit, please enter 0");
+        System.out.println();
+    }
+
+    private void runLevelTwoNavigation() {
+        System.out.println();
+        System.out.println("Enter task number of level 2:");
+        System.out.println("to choose  task 1, please enter 1");
+        System.out.println("to choose  task 2, please enter 2");
+        System.out.println("if you want exit, please enter 0");
+        System.out.println();
+    }
+
+    private void numberOfTaskLevelOne(String position, BufferedReader reader) {
+        switch (position) {
+            case "1":
+                System.out.println("Task 1");
+                break;
+            case "2":
+                System.out.println("Task 1");
+                break;
+            case "3":
+                System.out.println("Task 3");
+                break;
+        }
+        runLevelOneNavigation();
+    }
+
+    private void numberOfTaskLevelTwo(String position, BufferedReader reader) {
+        switch (position) {
+            case "1":
+                System.out.println("Task 1");
+                break;
+            case "2":
+                System.out.println("Task 2");
+                break;
+        }
+        runLevelTwoNavigation();
+    }
+}
