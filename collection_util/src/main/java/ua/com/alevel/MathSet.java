78d3b56a298d201d0bd913d69cbb50fdbd64f0b2
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-11-08 15:46:38 +0200

    added constructors to MathSet

--- collection_util/src/main/java/ua/com/alevel/MathSet.java
+++ collection_util/src/main/java/ua/com/alevel/MathSet.java
@@ -1,23 +1,145 @@
 package ua.com.alevel;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public final class MathSet {
 
     private Number[] setArray = new Number[2];
-    private static int capacity = 0;
-    private static int countNumbers = 0;
+    private int maxCapacity = 0;
+    private int countNumbers = 0;
+    private final Map<String, Integer> typePriority = new HashMap<>();
 
     public MathSet() {
+        fillTypePriority();
+    }
 
+    public MathSet(int capacity) {
+        fillTypePriority();
+        maxCapacity = capacity;
+    }
+
+    public MathSet(Number[] numbers) {
+        fillTypePriority();
+        addFromArray(numbers);
+    }
+
+    public MathSet(Number[] ... numbers) {
+        fillTypePriority();
+        addFromArray(numbers);
     }
 
     public void add(Number number) {
-        setArray[countNumbers] = number;
-        countNumbers++;
-        if(countNumbers == setArray.length) {
-            setArray = Arrays.copyOf(setArray, countNumbers + 10);
+        if(number != null) {
+            addUniqueValueInArray(number);
+            checkAndIncreaseArray();
         }
     }
 
+    public void add(Number... numbers) {
+        for (Number number : numbers) {
+            add(number);
+        }
+    }
+
+    private void addFromArray(Number[] numbers) {
+        for (Number number : numbers) {
+            add(number);
+        }
+    }
+
+    private void addFromArray(Number[] ... arrayOfNumbers) {
+        for (Number[] numbers : arrayOfNumbers) {
+            for (Number number : numbers) {
+                add(number);
+            }
+        }
+    }
+
+    private void addUniqueValueInArray(Number number) {
+        for (int i = 0; i < setArray.length; i++) {
+            if (setArray[i] != null && compareNumbers(setArray[i], number, i)) {
+                return;
+            }
+        }
+        checkArrayIncreaseToCapacity(countNumbers + 1);
+        setArray[countNumbers] = number;
+        countNumbers++;
+    }
+
+    private boolean compareNumbers(Number numberInArray, Number number, int i) {
+        if (countNumbers == 0) {
+            return false;
+        }
+        Integer maxPriority = Math.max(typePriority.get(number.getClass().getSimpleName()), typePriority.get(numberInArray.getClass().getSimpleName()));
+        String nameClassForCompare = "";
+        for (Map.Entry entry : typePriority.entrySet()) {
+            if (entry.getValue() == maxPriority) {
+                nameClassForCompare = (String) entry.getKey();
+                break;
+            }
+        }
+        switch (nameClassForCompare) {
+            case "Byte":
+                if (numberInArray.byteValue() == number.byteValue()) {
+                    setArray[i] = number.byteValue();
+                    return true;
+                }
+            case "Short":
+                if (numberInArray.shortValue() == number.shortValue()) {
+                    setArray[i] = number.shortValue();
+                    return true;
+                }
+            case "Integer":
+                if (numberInArray.intValue() == number.intValue()) {
+                    setArray[i] = number.intValue();
+                    return true;
+                }
+            case "Long":
+                if (numberInArray.longValue() == number.longValue()) {
+                    setArray[i] = number.longValue();
+                    return true;
+                }
+            case "Float":
+                if (numberInArray.floatValue() == number.floatValue()) {
+                    setArray[i] = number.floatValue();
+                    return true;
+                }
+            case "Double":
+                if (numberInArray.doubleValue() == number.doubleValue()) {
+                    setArray[i] = number.doubleValue();
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    private void fillTypePriority() {
+        typePriority.put("Byte", 0);
+        typePriority.put("Short", 1);
+        typePriority.put("Integer", 2);
+        typePriority.put("Long", 3);
+        typePriority.put("Float", 4);
+        typePriority.put("Double", 5);
+    }
+
+    private void checkArrayIncreaseToCapacity(int newCountNumbers) {
+        if (maxCapacity != 0 && newCountNumbers > maxCapacity) {
+            throw new RuntimeException("Current capacity: " + maxCapacity + "; new count of numbers: " + newCountNumbers + "; before adding you need to increase capacity!");
+        }
+    }
+
+    private void checkAndIncreaseArray() {
+        if (countNumbers == setArray.length) {
+            setArray = increaseArray(setArray, countNumbers + 10);
+        }
+    }
+
+    private Number[] increaseArray(Number[] currentArray, int newLength) {
+        Number[] newArray = new Number[newLength];
+        for (int i = 0; i < currentArray.length; i++) {
+            newArray[i] = currentArray[i];
+        }
+        return newArray;
+    }
 }
--- hw_5_collection/src/main/java/ua/com/alevel/MathSetInterfaceMain.java
+++ hw_5_collection/src/main/java/ua/com/alevel/MathSetInterfaceMain.java
@@ -5,9 +5,42 @@ public class MathSetInterfaceMain {
     public static void main(String[] args) {
 
         MathSet mathSet = new MathSet();
+        mathSet.add((byte)1);
+        mathSet.add((short)1);
         mathSet.add(1);
+        mathSet.add(1.0);
+        mathSet.add(1l);
         mathSet.add(0.2);
         mathSet.add(7);
+        mathSet.add(7,2,3,4,5);
+
+        MathSet mathSetCapacity = new MathSet(3);
+        mathSetCapacity.add((byte)1);
+        mathSetCapacity.add((short)1);
+        mathSetCapacity.add(1);
+        mathSetCapacity.add(1.0);
+        mathSetCapacity.add(1l);
+        mathSetCapacity.add(0.2);
+
+        Number[] numbers = new Number[4];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 3;
+        MathSet mathSetNumbersArray = new MathSet(numbers);
+
+        Number[] numbers2 = new Number[4];
+        numbers2[0] = 2;
+        numbers2[1] = 3;
+        numbers2[2] = 4;
+        numbers2[3] = 5;
+
+        Number[] numbers3 = new Number[4];
+        numbers3[0] = 3;
+        numbers3[1] = 4;
+        numbers3[2] = 5;
+        numbers3[3] = 6;
+        MathSet mathSetNumbersArray2 = new MathSet(numbers, numbers2, numbers3);
 
     }
 }
