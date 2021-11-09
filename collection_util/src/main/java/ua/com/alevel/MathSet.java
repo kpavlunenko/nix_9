4eff19bcff654246678928dcbc113c967fd4dded
Kostiantyn Pavlunenko <kpavlunenko@gmail.com>
2021-11-09 12:08:03 +0200

    added procedures sortDesc, sortAsc

--- collection_util/src/main/java/ua/com/alevel/MathSet.java
+++ collection_util/src/main/java/ua/com/alevel/MathSet.java
@@ -24,7 +24,7 @@ public final class MathSet {
         addFromArray(numbers);
     }
 
-    public MathSet(Number[] ... numbers) {
+    public MathSet(Number[]... numbers) {
         fillTypePriority();
         for (Number[] number : numbers) {
             addFromArray(number);
@@ -36,7 +36,7 @@ public final class MathSet {
         addFromArray(mathSet.setArray);
     }
 
-    public MathSet(MathSet ... mathSets) {
+    public MathSet(MathSet... mathSets) {
         fillTypePriority();
         for (MathSet mathSet : mathSets) {
             addFromArray(mathSet.setArray);
@@ -44,13 +44,13 @@ public final class MathSet {
     }
 
     public void add(Number number) {
-        if(number != null) {
+        if (number != null) {
             addUniqueValueInArray(number);
             checkAndIncreaseArray();
         }
     }
 
-    public void add(Number ... numbers) {
+    public void add(Number... numbers) {
         addFromArray(numbers);
     }
 
@@ -60,38 +60,9 @@ public final class MathSet {
         }
     }
 
-    public void join(MathSet mathSet) {
-        addFromArray(mathSet.setArray);
-    }
-
-    public void join(MathSet ... mathSets) {
-        for (MathSet mathSet : mathSets) {
-            addFromArray(mathSet.setArray);
-        }
-    }
-
-    public void intersection(MathSet mathSet) {
-        Number[] oldSetArray = setArray;
-        setArray = new Number[Math.max(mathSet.setArray.length, countNumbers)];
-        countNumbers = 0;
-        for (Number number : mathSet.setArray) {
-            for (Number oldNumber : oldSetArray) {
-                if (number != null && oldNumber != null && compareNumbersAndAddInArray(oldNumber, number, countNumbers)) {
-                    countNumbers++;
-                }
-            }
-        }
-    }
-
-    public void intersection(MathSet ... mathSets) {
-        for (MathSet mathSet : mathSets) {
-            intersection(mathSet);
-        }
-    }
-
     private void addUniqueValueInArray(Number number) {
-        for (int i = 0; i < setArray.length; i++) {
-            if (setArray[i] != null && compareNumbersAndAddInArray(setArray[i], number, i)) {
+        for (int i = 0; i < countNumbers; i++) {
+            if (setArray[i] != null && compareNumbersAndAddInArray(setArray[i], number, i, true)) {
                 return;
             }
         }
@@ -100,7 +71,117 @@ public final class MathSet {
         countNumbers++;
     }
 
-    private boolean compareNumbersAndAddInArray(Number numberInArray, Number number, int i) {
+    public void join(MathSet mathSet) {
+        addFromArray(mathSet.setArray);
+    }
+
+    public void join(MathSet... mathSets) {
+        for (MathSet mathSet : mathSets) {
+            addFromArray(mathSet.setArray);
+        }
+    }
+
+    public void intersection(MathSet mathSet) {
+        Number[] oldSetArray = setArray;
+        setArray = new Number[Math.max(mathSet.countNumbers, countNumbers)];
+        countNumbers = 0;
+        intersectionProcedure(mathSet, oldSetArray);
+    }
+
+    public void intersection(MathSet... mathSets) {
+        for (MathSet mathSet : mathSets) {
+            intersection(mathSet);
+        }
+    }
+
+    private void intersectionProcedure(MathSet mathSet, Number[] oldSetArray) {
+        for (Number number : mathSet.setArray) {
+            for (Number oldNumber : oldSetArray) {
+                if (number != null && oldNumber != null && compareNumbersAndAddInArray(oldNumber, number, countNumbers, true)) {
+                    countNumbers++;
+                }
+            }
+        }
+    }
+
+    public void sortDesc() {
+        for (int left = 0; left < countNumbers; left++) {
+            int minInd = left;
+            for (int i = left; i < countNumbers; i++) {
+                if (numberIsLessThanTheSecond(setArray[minInd], setArray[i])) {
+                    minInd = i;
+                }
+            }
+            swap(setArray, minInd, left);
+        }
+    }
+
+    public void sortDesc(int firstIndex, int lastIndex) {
+        firstIndex = Math.min(Math.max(firstIndex - 1, 0), countNumbers - 1);
+        lastIndex = Math.min(lastIndex, countNumbers);
+        for (int left = firstIndex; left < lastIndex; left++) {
+            int minInd = left;
+            for (int i = left; i < lastIndex; i++) {
+                if (numberIsLessThanTheSecond(setArray[minInd], setArray[i])) {
+                    minInd = i;
+                }
+            }
+            swap(setArray, minInd, left);
+        }
+    }
+
+    public void sortDesc(Number value) {
+        int indexInArray = getIndexOfNumber(value);
+        sortDesc(indexInArray + 1, countNumbers);
+    }
+
+    public void sortAsc() {
+        for (int left = 0; left < countNumbers; left++) {
+            int minInd = left;
+            for (int i = left; i < countNumbers; i++) {
+                if (numberIsLessThanTheSecond(setArray[i], setArray[minInd])) {
+                    minInd = i;
+                }
+            }
+            swap(setArray, left, minInd);
+        }
+    }
+
+    public void sortAsc(int firstIndex, int lastIndex) {
+        firstIndex = Math.min(Math.max(firstIndex - 1, 0), countNumbers - 1);
+        lastIndex = Math.min(lastIndex, countNumbers);
+        for (int left = firstIndex; left < lastIndex; left++) {
+            int minInd = left;
+            for (int i = left; i < lastIndex; i++) {
+                if (numberIsLessThanTheSecond(setArray[i], setArray[minInd])) {
+                    minInd = i;
+                }
+            }
+            swap(setArray, left, minInd);
+        }
+    }
+
+    public void sortAsc(Number value) {
+        int indexInArray = getIndexOfNumber(value);
+        sortAsc(indexInArray + 1, countNumbers);
+    }
+
+    private static void swap(Number[] array, int ind1, int ind2) {
+        Number tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
+    }
+
+    private int getIndexOfNumber(Number number) {
+        for (int i = 0; i < countNumbers; i++) {
+            if (setArray[i] != null && compareNumbersAndAddInArray(setArray[i], number, i, false)) {
+                return i;
+            }
+        }
+        return countNumbers;
+    }
+
+    private boolean compareNumbersAndAddInArray(Number numberInArray, Number number, int i, boolean changeValue) {
         Integer maxPriority = Math.max(TYPE_PRIORITY.get(number.getClass().getSimpleName()), TYPE_PRIORITY.get(numberInArray.getClass().getSimpleName()));
         String nameClassForCompare = "";
         for (Map.Entry entry : TYPE_PRIORITY.entrySet()) {
@@ -112,32 +193,82 @@ public final class MathSet {
         switch (nameClassForCompare) {
             case "Byte":
                 if (numberInArray.byteValue() == number.byteValue()) {
-                    setArray[i] = number.byteValue();
+                    if (changeValue) {
+                        setArray[i] = number.byteValue();
+                    }
                     return true;
                 }
             case "Short":
                 if (numberInArray.shortValue() == number.shortValue()) {
-                    setArray[i] = number.shortValue();
+                    if (changeValue) {
+                        setArray[i] = number.shortValue();
+                    }
                     return true;
                 }
             case "Integer":
                 if (numberInArray.intValue() == number.intValue()) {
-                    setArray[i] = number.intValue();
+                    if (changeValue) {
+                        setArray[i] = number.intValue();
+                    }
                     return true;
                 }
             case "Long":
                 if (numberInArray.longValue() == number.longValue()) {
-                    setArray[i] = number.longValue();
+                    if (changeValue) {
+                        setArray[i] = number.longValue();
+                    }
                     return true;
                 }
             case "Float":
                 if (numberInArray.floatValue() == number.floatValue()) {
-                    setArray[i] = number.floatValue();
+                    if (changeValue) {
+                        setArray[i] = number.floatValue();
+                    }
                     return true;
                 }
             case "Double":
                 if (numberInArray.doubleValue() == number.doubleValue()) {
-                    setArray[i] = number.doubleValue();
+                    if (changeValue) {
+                        setArray[i] = number.doubleValue();
+                    }
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    private boolean numberIsLessThanTheSecond(Number firstNumber, Number secondNumber) {
+        Integer maxPriority = Math.max(TYPE_PRIORITY.get(firstNumber.getClass().getSimpleName()), TYPE_PRIORITY.get(secondNumber.getClass().getSimpleName()));
+        String nameClassForCompare = "";
+        for (Map.Entry entry : TYPE_PRIORITY.entrySet()) {
+            if (entry.getValue() == maxPriority) {
+                nameClassForCompare = (String) entry.getKey();
+                break;
+            }
+        }
+        switch (nameClassForCompare) {
+            case "Byte":
+                if (firstNumber.byteValue() < secondNumber.byteValue()) {
+                    return true;
+                }
+            case "Short":
+                if (firstNumber.shortValue() < secondNumber.shortValue()) {
+                    return true;
+                }
+            case "Integer":
+                if (firstNumber.intValue() < secondNumber.intValue()) {
+                    return true;
+                }
+            case "Long":
+                if (firstNumber.longValue() < secondNumber.longValue()) {
+                    return true;
+                }
+            case "Float":
+                if (firstNumber.floatValue() < secondNumber.floatValue()) {
+                    return true;
+                }
+            case "Double":
+                if (firstNumber.doubleValue() < secondNumber.doubleValue()) {
                     return true;
                 }
         }
--- hw_5_collection/src/main/java/ua/com/alevel/MathSetInterfaceMain.java
+++ hw_5_collection/src/main/java/ua/com/alevel/MathSetInterfaceMain.java
@@ -4,60 +4,18 @@ public class MathSetInterfaceMain {
 
     public static void main(String[] args) {
 
-        MathSet mathSet = new MathSet();
-        mathSet.add((byte)1);
-        mathSet.add((short)1);
-        mathSet.add(1);
-        mathSet.add(1.0);
-        mathSet.add(1l);
-        mathSet.add(0.2);
-        mathSet.add(7);
-        mathSet.add(7,2,3,4,5);
+        Number[] numbers5 = new Number[5];
+        numbers5[0] = 6;
+        numbers5[1] = 5l;
+        numbers5[2] = 4.0;
+        numbers5[3] = 4.1;
+        numbers5[4] = (byte)7;
+        MathSet mathSetNumber5 = new MathSet(numbers5);
 
-        MathSet mathSetCapacity = new MathSet(3);
-        mathSetCapacity.add((byte)1);
-        mathSetCapacity.add((short)1);
-        mathSetCapacity.add(1);
-        mathSetCapacity.add(1.0);
-        mathSetCapacity.add(1l);
-        mathSetCapacity.add(0.2);
-
-        Number[] numbers = new Number[4];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        numbers[2] = 3;
-        numbers[3] = 3;
-        MathSet mathSetNumbersArray = new MathSet(numbers);
-
-        Number[] numbers2 = new Number[4];
-        numbers2[0] = 2;
-        numbers2[1] = 3;
-        numbers2[2] = 4;
-        numbers2[3] = 5;
-
-        Number[] numbers3 = new Number[4];
-        numbers3[0] = 3;
-        numbers3[1] = 4;
-        numbers3[2] = 5;
-        numbers3[3] = 6;
-        MathSet mathSetNumbersArray2 = new MathSet(numbers, numbers2, numbers3);
-
-        MathSet mathSetFromMathSet = new MathSet(mathSetNumbersArray2);
-        MathSet mathSetFromMathSetArrays = new MathSet(mathSet, mathSetCapacity, mathSetNumbersArray, mathSetNumbersArray2, mathSetFromMathSet);
-
-        MathSet mathSetJoin = new MathSet(numbers3);
-        mathSetJoin.join(mathSetNumbersArray);
-        mathSetJoin.join(mathSet, mathSetCapacity, mathSetNumbersArray, mathSetNumbersArray2, mathSetFromMathSet);
-
-        MathSet mathSetNumber2 = new MathSet(numbers2);
-        MathSet mathSetIntersection = new MathSet(numbers3);
-        mathSetIntersection.intersection(mathSetNumber2);
-
-        MathSet mathSetNumber3 = new MathSet(numbers3);
-        MathSet mathSetIntersection2 = new MathSet(numbers3);
-        MathSet mathSet4 = new MathSet();
-        mathSet4.add(4l);
-        mathSetIntersection2.intersection(mathSetNumber2, mathSetNumber3, mathSet4);
+        mathSetNumber5.sortAsc(5);
+        mathSetNumber5.sortAsc(2, 4);
+        mathSetNumber5.sortAsc(10, 10);
+        mathSetNumber5.sortAsc();
 
     }
 }
